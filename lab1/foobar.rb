69e4047de0247078b21a06d69e61a0057992b178
Denis Mukha <dionysiusmukha@gmail.com>
2025-07-07 11:38:20 +0300

    Added foobar.rb for lab1

---
+++ lab1/foobar.rb
@@ -0,0 +1,13 @@
+def foobar()
+  puts "Введите первое число"
+  firstNumber = gets.to_i
+  puts "Введите второе число"
+  secondNumber = gets.to_i
+  if firstNumber == 20 or secondNumber == 20
+    return secondNumber
+  else
+    return firstNumber + secondNumber
+  end
+end
+
+print foobar
